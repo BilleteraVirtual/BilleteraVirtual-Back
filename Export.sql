8bcef891d69574e5d9475a4b31d2975f4b4b6444
FranchuConAldaxtutu <alumno25.arias.francisco@ipm.edu.ar>
2024-08-15 17:21:16 -0300

    Export DataBase

---
+++ Export.sql
@@ -0,0 +1,193 @@
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Billetera
+-- ------------------------------------------------------
+-- Server version	8.0.39-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Categoria`
+--
+
+DROP TABLE IF EXISTS `Categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Categoria` (
+  `idCategoria` int NOT NULL,
+  `Tipo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Categoria`
+--
+
+LOCK TABLES `Categoria` WRITE;
+/*!40000 ALTER TABLE `Categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Empresa`
+--
+
+DROP TABLE IF EXISTS `Empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Empresa` (
+  `idEmpresa` int NOT NULL,
+  `RazonSocial` varchar(45) DEFAULT NULL,
+  `Categoria_idCategoria` int NOT NULL,
+  `Entidad_CVU` int NOT NULL,
+  PRIMARY KEY (`idEmpresa`,`Categoria_idCategoria`,`Entidad_CVU`),
+  KEY `fk_Empresa_Categoria2_idx` (`Categoria_idCategoria`),
+  KEY `fk_Empresa_Entidad1_idx` (`Entidad_CVU`),
+  CONSTRAINT `fk_Empresa_Categoria2` FOREIGN KEY (`Categoria_idCategoria`) REFERENCES `Categoria` (`idCategoria`),
+  CONSTRAINT `fk_Empresa_Entidad1` FOREIGN KEY (`Entidad_CVU`) REFERENCES `Entidad` (`CVU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Empresa`
+--
+
+LOCK TABLES `Empresa` WRITE;
+/*!40000 ALTER TABLE `Empresa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Empresa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Entidad`
+--
+
+DROP TABLE IF EXISTS `Entidad`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Entidad` (
+  `CVU` int NOT NULL,
+  `Alias` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `Saldo` double DEFAULT NULL,
+  `Mail` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`CVU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Entidad`
+--
+
+LOCK TABLES `Entidad` WRITE;
+/*!40000 ALTER TABLE `Entidad` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Entidad` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Reserva`
+--
+
+DROP TABLE IF EXISTS `Reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Reserva` (
+  `idReserva` int NOT NULL,
+  `DNIUsuario` int DEFAULT NULL,
+  `Motivo` varchar(45) DEFAULT NULL,
+  `Monto` double DEFAULT NULL,
+  `Entidad_CVU` int NOT NULL,
+  PRIMARY KEY (`idReserva`,`Entidad_CVU`),
+  KEY `fk_Reserva_Entidad1_idx` (`Entidad_CVU`),
+  CONSTRAINT `fk_Reserva_Entidad1` FOREIGN KEY (`Entidad_CVU`) REFERENCES `Entidad` (`CVU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reserva`
+--
+
+LOCK TABLES `Reserva` WRITE;
+/*!40000 ALTER TABLE `Reserva` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Transaccion`
+--
+
+DROP TABLE IF EXISTS `Transaccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Transaccion` (
+  `idTransaccion` int NOT NULL,
+  `Monto` varchar(45) DEFAULT NULL,
+  `Motivo` varchar(45) DEFAULT NULL,
+  `Fecha` datetime DEFAULT NULL,
+  `Categoria_idCategoria` int NOT NULL,
+  `Emisor_CVU` int NOT NULL,
+  `Destinatario_CVU` int NOT NULL,
+  PRIMARY KEY (`idTransaccion`,`Categoria_idCategoria`,`Emisor_CVU`,`Destinatario_CVU`),
+  KEY `fk_Transaccion_Categoria1_idx` (`Categoria_idCategoria`),
+  KEY `fk_Transaccion_Entidad1_idx` (`Emisor_CVU`),
+  KEY `fk_Transaccion_Entidad2_idx` (`Destinatario_CVU`),
+  CONSTRAINT `fk_Transaccion_Categoria1` FOREIGN KEY (`Categoria_idCategoria`) REFERENCES `Categoria` (`idCategoria`),
+  CONSTRAINT `fk_Transaccion_Entidad1` FOREIGN KEY (`Emisor_CVU`) REFERENCES `Entidad` (`CVU`),
+  CONSTRAINT `fk_Transaccion_Entidad2` FOREIGN KEY (`Destinatario_CVU`) REFERENCES `Entidad` (`CVU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Transaccion`
+--
+
+LOCK TABLES `Transaccion` WRITE;
+/*!40000 ALTER TABLE `Transaccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Transaccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Usuario`
+--
+
+DROP TABLE IF EXISTS `Usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Usuario` (
+  `DNI` int NOT NULL,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Usuariocol` varchar(45) DEFAULT NULL,
+  `Entidad_CVU` int NOT NULL,
+  PRIMARY KEY (`DNI`,`Entidad_CVU`),
+  KEY `fk_Usuario_Entidad_idx` (`Entidad_CVU`),
+  CONSTRAINT `fk_Usuario_Entidad` FOREIGN KEY (`Entidad_CVU`) REFERENCES `Entidad` (`CVU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Usuario`
+--
+
+LOCK TABLES `Usuario` WRITE;
+/*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-15 17:14:54
