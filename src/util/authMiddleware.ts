db61857a5f578f676f29767289affff6148a040a
juanchi0207 <juanchi.sampieri@gmail.com>
2024-11-18 20:07:22 -0300

    Agregamos metodo para obtener los detalles de cada entidad basado es su tipo obtenido del token

--- src/util/authMiddleware.ts
+++ src/util/authMiddleware.ts
@@ -2,21 +2,21 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
-const checkToken = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['authorization'];
-
+export function checkToken(req: Request, res: Response, next: NextFunction): void {
+  const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    return res.status(403).json({ message: 'Token is required' });
+      res.status(401).json({ message: 'No token provided' });
+      return;
   }
 
-  // Validar el token usando la clave secreta de las variables de entorno
-  jwt.verify(token.split(' ')[1], process.env.JWT_SECRET || 'default_secret', (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-    
-    next();
-  });
-};
+  try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || 'default_secret') as { cvu: string; type: string };
+      req.body.cvu = decoded.cvu; // Añadir al cuerpo
+      req.body.type = decoded.type; // Añadir al cuerpo
+      next();
+  } catch (error) {
+      res.status(401).json({ message: 'Invalid or expired token' });
+  }
+}
 
 export default checkToken;
